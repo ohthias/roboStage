922b42313b11eb9bf2d14c6c203e978ababd129e
Matheus Gabriel <math.gsilva07@gmail.com>
2025-05-10 23:35:36 -0300

    Update package-lock.json to include hasInstallScript and refactor prisma.js for better global handling. Remove deprecated salasDB.js file.

--- robo-stage/src/app/lib/prisma.js
+++ robo-stage/src/app/lib/prisma.js
@@ -1,8 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis;
+globalForPrisma.prisma = globalForPrisma.prisma || undefined;
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 

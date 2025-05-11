82ae9adbd74337761d71cf59d685c3cb1456db9a
Matheus Gabriel <math.gsilva07@gmail.com>
2025-05-11 10:35:59 -0300

    Summary

--- robo-stage/src/app/lib/prisma.js
+++ robo-stage/src/app/lib/prisma.js
@@ -5,4 +5,8 @@ globalForPrisma.prisma = globalForPrisma.prisma || undefined;
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
+if (!globalForPrisma.prisma) {
+  globalForPrisma.prisma = prisma;
+}
+
 export default prisma;

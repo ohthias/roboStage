8d3f8398dc38d2d1841fbbd29d34426eb7ac4ea1
Matheus Gabriel <math.gsilva07@gmail.com>
2025-05-12 21:43:50 -0300

    Refactor prisma.js to simplify global PrismaClient assignment and improve production environment handling.

--- robo-stage/src/app/lib/prisma.js
+++ robo-stage/src/app/lib/prisma.js
@@ -1,11 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis;
-globalForPrisma.prisma = globalForPrisma.prisma || undefined;
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (!globalForPrisma.prisma) {
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
 

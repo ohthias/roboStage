0f7a243e6e5264820b7d7088a13c8cad90ce850b
Matheus Gabriel <math.gsilva07@gmail.com>
2025-07-10 13:41:53 -0300

    Add QuickBrick drawing tool with layer management and various drawing functionalities

--- app/page.tsx
+++ app/page.tsx
@@ -1,118 +1,171 @@
 "use client";
-import { useState, useEffect } from "react";
-import FormMission from "@/components/FormMission";
-import Loader from "@/components/loader";
-import { calculateTotalPoints } from "@/utils/calculateTotalPoints";
+
 import Banner from "@/components/Banner";
 import Hero from "@/components/hero";
 import Footer from "@/components/ui/Footer";
 
-type MissionType = {
-  id: string;
-  name: string;
-  mission?: string;
-  type: string[];
-  points: number[] | number;
-  "sub-mission"?: {
-    submission: string;
-    type: string[];
-  }[];
-};
-
-type ResponseType = {
-  [missionId: string]: {
-    [index: number]: string | number;
-  };
-};
-
 export default function Home() {
-  const [missions, setMissions] = useState<MissionType[]>([]);
-  const [responses, setResponses] = useState<ResponseType>({});
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    fetch("/data/missions.json")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Erro ao buscar as missões");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setMissions(data.missions);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Erro:", error);
-        setLoading(false);
-      });
-  }, []);
-
-  const handleSelect = (
-    missionId: string,
-    questionIndex: number,
-    value: string | number
-  ) => {
-    setResponses((prev) => ({
-      ...prev,
-      [missionId]: {
-        ...prev[missionId],
-        [questionIndex]: value,
-      },
-    }));
-  };
-
-  const totalPoints = calculateTotalPoints(missions, responses);
-
-  if (loading) {
-    return (
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "100vh",
-          backgroundColor: "rgba(0, 0, 0, 0.8)",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          zIndex: 9999,
-          backdropFilter: "blur(5px)",
-        }}
-      >
-        <Loader />
-      </div>
-    );
-  }
-
   return (
     <>
       <Hero />
       <Banner />
-      <main className="flex flex-col items-center justify-center gap-8 px-4 py-16 sm:px-6 lg:px-8">
-        <div className="flex flex-col sm:flex-row items-center justify-between gap-4 relative my-4 w-full max-w-4xl">
-          <div className="flex-1 flex justify-center sm:justify-start text-center sm:text-left">
-            <h1
-              className="text-3xl sm:text-4xl lg:text-5xl font-bold text-primary"
-              id="pontuador"
-            >
-              Pontuador
-            </h1>
+      <main className="flex flex-col items-center justify-center gap-8 px-4 py-0 sm:px-6 lg:px-8">
+        <div className="py-24 sm:py-12 mb-12">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:text-center">
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl lg:text-balance">
+                Everything you need to deploy your app
+              </p>
+              <p className="mt-6 text-lg/8 text-gray-700">
+                Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
+                Suspendisse eget egestas a elementum pulvinar et feugiat blandit
+                at. In mi viverra elit nunc.
+              </p>
+            </div>
+            <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+              <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                <div className="relative pl-16">
+                  <dt className="text-base/7 font-semibold text-gray-900">
+                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                      <svg
+                        className="size-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
+                        />
+                      </svg>
+                    </div>
+                    Push to deploy
+                  </dt>
+                  <dd className="mt-2 text-base/7 text-gray-600">
+                    Morbi viverra dui mi arcu sed. Tellus semper adipiscing
+                    suspendisse semper morbi. Odio urna massa nunc massa.
+                  </dd>
+                </div>
+                <div className="relative pl-16">
+                  <dt className="text-base/7 font-semibold text-gray-900">
+                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                      <svg
+                        className="size-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                        />
+                      </svg>
+                    </div>
+                    SSL certificates
+                  </dt>
+                  <dd className="mt-2 text-base/7 text-gray-600">
+                    Sit quis amet rutrum tellus ullamcorper ultricies libero
+                    dolor eget. Sem sodales gravida quam turpis enim lacus amet.
+                  </dd>
+                </div>
+                <div className="relative pl-16">
+                  <dt className="text-base/7 font-semibold text-gray-900">
+                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                      <svg
+                        className="size-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                        />
+                      </svg>
+                    </div>
+                    Simple queues
+                  </dt>
+                  <dd className="mt-2 text-base/7 text-gray-600">
+                    Quisque est vel vulputate cursus. Risus proin diam nunc
+                    commodo. Lobortis auctor congue commodo diam neque.
+                  </dd>
+                </div>
+                <div className="relative pl-16">
+                  <dt className="text-base/7 font-semibold text-gray-900">
+                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                      <svg
+                        className="size-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 0 1-3.6 9.75m6.633-4.596a18.666 18.666 0 0 1-2.485 5.33"
+                        />
+                      </svg>
+                    </div>
+                    Advanced security
+                  </dt>
+                  <dd className="mt-2 text-base/7 text-gray-600">
+                    Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt
+                    mattis aliquet hac quis. Id hac maecenas ac donec pharetra
+                    eget.
+                  </dd>
+                </div>
+              </dl>
+            </div>
           </div>
+        </div>
+      </main>
 
-          <div className="bg-light-smoke flex flex-col items-center justify-center rounded-md shadow-md p-4 w-full sm:w-auto max-w-[200px]">
-            <p className="text-sm font-bold text-black">Pontos</p>
-            <h3 className="text-2xl font-bold text-primary">{totalPoints}</h3>
+      <section className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center dark:bg-gray-900">
+        <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+          <div className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+            <h2 className="text-2xl font-bold text-gray-900 md:text-3xl dark:text-white">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit
+            </h2>
+
+            <p className="hidden text-gray-500 md:mt-4 md:block dark:text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
+              egestas tempus tellus etiam sed. Quam a scelerisque amet
+              ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat
+              quisque ut interdum tincidunt duis.
+            </p>
+
+            <div className="mt-4 md:mt-8">
+              <a
+                href="#"
+                className="inline-block rounded-sm bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:ring-3 focus:ring-yellow-400 focus:outline-hidden"
+              >
+                Get Started Today
+              </a>
+            </div>
           </div>
         </div>
 
-        <FormMission
-          missions={missions}
-          responses={responses}
-          onSelect={handleSelect}
+        <img
+          alt=""
+          src="https://images.unsplash.com/photo-1484959014842-cd1d967a39cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[60px]"
         />
-      </main>
+      </section>
       <Footer />
     </>
   );

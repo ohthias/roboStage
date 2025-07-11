864aa6f6d8f660e3b66a4c21d0b16ddaf224af32
Matheus Gabriel <math.gsilva07@gmail.com>
2025-07-11 15:12:31 -0300

    Refactor Home page layout and content for better clarity and user engagement; update text, styles, and links to enhance the overall experience.

--- app/page.tsx
+++ app/page.tsx
@@ -9,124 +9,67 @@ export default function Home() {
     <>
       <Hero />
       <Banner />
-      <main className="flex flex-col items-center justify-center gap-8 px-4 py-0 sm:px-6 lg:px-8">
+      <main className="flex flex-col items-center justify-center gap-8">
         <div className="py-24 sm:py-12 mb-12">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl lg:text-center">
-              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl lg:text-balance">
-                Everything you need to deploy your app
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-teal-500 sm:text-5xl lg:text-balance">
+                Potencialize sua equipe de robótica
               </p>
-              <p className="mt-6 text-lg/8 text-gray-700">
-                Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
-                Suspendisse eget egestas a elementum pulvinar et feugiat blandit
-                at. In mi viverra elit nunc.
+              <p className="mt-6 text-lg/8 text-zinc-900">
+                Crie estratégias, registre pontuações, conduza testes e
+                documente cada avanço do seu robô. Além disso, organize eventos
+                completos de robótica com facilidade.
               </p>
             </div>
             <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
               <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
                 <div className="relative pl-16">
-                  <dt className="text-base/7 font-semibold text-gray-900">
-                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
-                      <svg
-                        className="size-6 text-white"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                        data-slot="icon"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
-                        />
-                      </svg>
+                  <dt className="text-base/7 font-semibold text-zinc-900">
+                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-teal-500 text-white">
+                      <i className="fi fi-rr-hundred-points" style={{ lineHeight: 0}}></i>
                     </div>
-                    Push to deploy
+                    FLL Score
                   </dt>
-                  <dd className="mt-2 text-base/7 text-gray-600">
-                    Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                    suspendisse semper morbi. Odio urna massa nunc massa.
+                  <dd className="mt-2 text-base/7 text-zinc-600">
+                    Registre e acompanhe o desempenho do seu robô na arena da
+                    temporada UNEARTHED de forma prática
                   </dd>
                 </div>
                 <div className="relative pl-16">
-                  <dt className="text-base/7 font-semibold text-gray-900">
-                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
-                      <svg
-                        className="size-6 text-white"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                        data-slot="icon"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
-                        />
-                      </svg>
+                  <dt className="text-base/7 font-semibold text-zinc-900">
+                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-red-600 text-white">
+                      <i className="fi fi-rr-blueprint" style={{ lineHeight: 0}}></i>
                     </div>
-                    SSL certificates
+                    Quick<span className="text-red-600">Brick</span> Studio
                   </dt>
-                  <dd className="mt-2 text-base/7 text-gray-600">
-                    Sit quis amet rutrum tellus ullamcorper ultricies libero
-                    dolor eget. Sem sodales gravida quam turpis enim lacus amet.
+                  <dd className="mt-2 text-base/7 text-zinc-600">
+                    Desenhe estratégias visuais para cumprir missões da FLL
+                    UNEARTHED ou desafios de resgate da OBR.
                   </dd>
                 </div>
                 <div className="relative pl-16">
-                  <dt className="text-base/7 font-semibold text-gray-900">
-                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
-                      <svg
-                        className="size-6 text-white"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                        data-slot="icon"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-                        />
-                      </svg>
+                  <dt className="text-base/7 font-semibold text-zinc-900 flex justify-start items-center">
+                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-red-600 text-white">
+                      <i className="fi fi-rr-blood-test-tube-alt" style={{ lineHeight: 0}}></i>
                     </div>
-                    Simple queues
+                    lab<span className="text-red-600">Test</span><div className="ml-2 p-1 text-xs text-yellow-600 font-semibold bg-yellow-100 w-max rounded-full">Em desenvolvimento</div>
                   </dt>
-                  <dd className="mt-2 text-base/7 text-gray-600">
-                    Quisque est vel vulputate cursus. Risus proin diam nunc
-                    commodo. Lobortis auctor congue commodo diam neque.
+                  <dd className="mt-2 text-base/7 text-zinc-600">
+                    Simule missões específicas, saídas ou rounds completos e
+                    analise resultados detalhados do seu robô.
                   </dd>
                 </div>
                 <div className="relative pl-16">
-                  <dt className="text-base/7 font-semibold text-gray-900">
-                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
-                      <svg
-                        className="size-6 text-white"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                        data-slot="icon"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 0 1-3.6 9.75m6.633-4.596a18.666 18.666 0 0 1-2.485 5.33"
-                        />
-                      </svg>
+                  <dt className="text-base/7 font-semibold text-zinc-900">
+                    <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-teal-500 text-white">
+                      <i className="fi fi-rr-stage-theatre" style={{ lineHeight: 0}}></i>
                     </div>
-                    Advanced security
+                    show<span className="text-teal-500">Live</span>
                   </dt>
-                  <dd className="mt-2 text-base/7 text-gray-600">
-                    Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt
-                    mattis aliquet hac quis. Id hac maecenas ac donec pharetra
-                    eget.
+                  <dd className="mt-2 text-base/7 text-zinc-600">
+                    Organize eventos de robótica para até 10 equipes, defina
+                    formatos de competição, fases e permissões personalizadas.
                   </dd>
                 </div>
               </dl>
@@ -135,34 +78,33 @@ export default function Home() {
         </div>
       </main>
 
-      <section className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center dark:bg-gray-900">
-        <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+      <section className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center">
+        <div className="p-8 md:p-12 lg:px-10 lg:py-24">
           <div className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-            <h2 className="text-2xl font-bold text-gray-900 md:text-3xl dark:text-white">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit
+            <h2 className="text-2xl font-bold text-gray-600 md:text-3xl">
+              Totalmente imerso na robótica
             </h2>
 
-            <p className="hidden text-gray-500 md:mt-4 md:block dark:text-gray-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
-              egestas tempus tellus etiam sed. Quam a scelerisque amet
-              ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat
-              quisque ut interdum tincidunt duis.
+            <p className="hidden text-gray-500 md:mt-4 md:block">
+              Com o <span className="text-red-600">roboStage</span>, você amplia
+              a visão sobre o desempenho do seu time e cria eventos
+              personalizados de maneira simples e intuitiva.
             </p>
 
             <div className="mt-4 md:mt-8">
               <a
-                href="#"
-                className="inline-block rounded-sm bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:ring-3 focus:ring-yellow-400 focus:outline-hidden"
+                href="/create-room"
+                className="inline-block rounded-sm bg-red-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-red-700 focus:ring-3 focus:ring-yellow-400 focus:outline-hidden"
               >
-                Get Started Today
+                Explore hoje mesmo
               </a>
             </div>
           </div>
         </div>
 
         <img
-          alt=""
-          src="https://images.unsplash.com/photo-1484959014842-cd1d967a39cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          alt="FLL board game"
+          src="/images/index_image.jpg"
           className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[60px]"
         />
       </section>

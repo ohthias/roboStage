0da1cf93230ae44025a797477912b28e1514900d
Matheus Gabriel <math.gsilva07@gmail.com>
2025-07-17 20:44:14 -0300

    feat: update dependencies and enhance Tailwind configuration
    - Added new Heroui components to package.json dependencies.
    - Updated React and React DOM versions to 19.1.0.
    - Modified Tailwind configuration to include Heroui theme and enable dark mode.

---
+++ app/providers.tsx
@@ -0,0 +1,5 @@
+import { HeroUIProvider } from "@heroui/system";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <HeroUIProvider>{children}</HeroUIProvider>;
+}

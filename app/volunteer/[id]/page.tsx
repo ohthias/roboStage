bb36284247067833dfeb413e8e0e3ec8e2e08d44
Matheus Gabriel <math.gsilva07@gmail.com>
2025-08-07 12:27:07 -0300

    refactor: update VolunteerPage to use client-side rendering and extract codigoSala from params

--- app/volunteer/[id]/page.tsx
+++ app/volunteer/[id]/page.tsx
@@ -1,3 +1,9 @@
-export default function VisitPage({ params }: { params: { id: string } }) {
-  return <div>Bem-vindo ao evento como voluntário! ID: {params.id}</div>;
+"use client";
+import { useParams } from 'next/navigation';
+
+export default function VolunteerPage() {
+   const params = useParams();
+  const codigoSala = params?.codigo_sala as string;
+
+  return <div>Bem-vindo ao evento como voluntário! ID: {codigoSala}</div>;
 }

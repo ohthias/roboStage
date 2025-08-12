6013cae68bc82398bd9d7de38ecc5cfadf0dca17
Matheus Gabriel <math.gsilva07@gmail.com>
2025-08-12 01:06:57 +0000

    refactor: remove @heroui dependencies and integrate daisyui for UI components
    - Removed multiple @heroui dependencies from package.json.
    - Added daisyui as a dependency for enhanced UI theming.
    - Updated tailwind.config.ts to use daisyui and configured themes.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,15 @@
-import {heroui} from '@heroui/theme';
-
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [require("tailwindcss-animate"),heroui()],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "bumblebee", 
+      "night"
+    ],
+  },
 };

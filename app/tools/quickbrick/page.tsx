0afe0d716624e51beb11c972b4b84e07af629773
Matheus Gabriel <math.gsilva07@gmail.com>
2025-08-12 13:41:31 +0000

    feat: reorganizar a estrutura da página QuickBrick, melhorando a apresentação do título e descrição

--- app/tools/quickbrick/page.tsx
+++ app/tools/quickbrick/page.tsx
@@ -3,9 +3,20 @@ import QuickBrickCanvas from "@/components/QuickBrickCanva";
 
 export default function QuickBrickPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center">
       <Navbar />
-      <h1 className="text-2xl font-bold mb-4">Quick Brick Page</h1>
+      <div className="text-center my-4 w-5xl">
+        <h1 className="text-2xl font-bold my-4 text-primary">
+          QuickBrick Studio
+        </h1>
+        <p className="text-sm mb-2 text-base-content">
+          O QuickBrick Studio é uma ferramenta que ajuda sua equipe a criar
+          estratégias eficientes para o robô durante sua jornada no FIRST LEGO
+          League Challenge. Basta selecionar uma das ferramentas disponíveis e
+          desenhar diretamente sobre a imagem do tapete, planejando cada
+          movimento com precisão e facilidade.
+        </p>
+      </div>
       <QuickBrickCanvas />
     </div>
   );
